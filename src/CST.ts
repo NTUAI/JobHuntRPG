d7de50079d11ead6fd83cc53ce305aabb14b4a5e
哈斯 Seth Harding <gearladeldsvon@gmail.com>
2024-02-03 23:51:47 +0800

    Reorganizing based on scenes, loading scene almost finished

---
+++ src/CST.ts
@@ -0,0 +1,23 @@
+export const CST = {
+    SCENES: {
+        LOAD: "LOAD",
+        MENU: "MENU",
+        PLAY: "PLAY"
+    },
+    IMAGE: {
+        LOADING: "loading.png",
+        TITLE: "mainmenu.png",
+        PLAY: "start.png",
+        OPTIONS: "options.png",
+        QUIT: "quit.png"
+    },
+    AUDIO: {
+        TITLE_MUSIC: "title.mp3",
+        TITLE_AUDIO: "seagull_sea.mp3",
+        GAME_MUSIC: "celtic.mp3",
+        GAME_AUDIO: "ship.mp3"
+    },
+    SPRITE: {
+        CAT: "cat.png"
+    }
+}
--- src/scene/LoadingScene.ts
+++ src/scene/LoadingScene.ts
@@ -1,16 +1,81 @@
-import 'phaser'
-
+import {CST} from "../CST";
 export class LoadingScene extends Phaser.Scene {
     constructor() {
-        super('LoadingScene');
+        super({key: CST.SCENES.LOAD});
+    }
+
+    init() {}
+
+    loadImages() {
+        this.load.setPath("./assets/image");
+        for (let prop in CST.IMAGE) {
+            this.load.image(CST.IMAGE[prop], CST.IMAGE[prop]);
+        }
+    }
+
+    loadAudio() {
+        this.load.setPath("./assets/audio");
+        for (let prop in CST.AUDIO) {
+            this.load.audio(CST.AUDIO[prop], CST.AUDIO[prop]);
+        }
+    }
+
+    /*loadSprites(frameConfig?: Phaser.Loader.FileTypes.ImageFrameConfig) {
+        this.load.setPath("./assets/sprite");
+
+        for (let prop in CST.SPRITE) {
+            //@ts-ignore
+            this.load.spritesheet(CST.SPRITE[prop], CST.SPRITE[prop], frameConfig);
+        }
+    }*/
+
+    delay(ms: number) {
+        return new Promise( resolve => setTimeout(resolve, ms) );
     }
 
     preload() {
-        
-    }
+        /*this.load.spritesheet("anna", "./assets/sprite/anna.png", {frameHeight: 64, frameWidth: 64});
+        //load atlases
+        this.load.atlas("characters", "./assets/sprite/characters.png", "./assets/sprite/characters.json")
+        this.load.atlas("daze", "./assets/sprite/daze.png", "./assets/sprite/daze.json")
+        this.load.spritesheet("rapier", "./assets/sprite/WEAPON_rapier.png", {frameHeight: 192, frameWidth: 192});*/
 
-    create() {
-        // Add any additional setup for your loading scene
-        this.scene.start('MainMenuScene'); // Switch to your main menu scene when loading is complete
+        //load images, audio, spritesheets
+        this.loadImages();
+        this.loadAudio();
+        /*this.loadSprites({
+            frameHeight: 32,
+            frameWidth: 32
+        });*/
+
+        //create loading bar
+        let loadingBar = this.add.graphics({
+            fillStyle: {
+                color: 0x000000 //white
+            }
+        })
+
+        /*
+        Loader Events:
+            complete - when done loading everything
+            progress - loader number progress in decimal
+        */
+        this.load.on("progress", (percent: number) => {
+            loadingBar.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 50);
+            console.log(percent);
+        })
+
+        //simulate large load
+        for(let i = 0; i < 100; i++){
+            this.load.image('./assets/images/player.png')
+        }
+
+        this.load.on("complete", () => {
+            this.scene.start(CST.SCENES.MENU);
+        });
+
+        this.load.on("load", (file: Phaser.Loader.File) => {
+            console.log(file.src)
+        })
     }
 }
